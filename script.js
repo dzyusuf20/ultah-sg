694835aa1674589ba74ed735429772ddcc294649
dzyusuf20 <Yusufdeardo20@gmail.com>
2024-11-26 00:31:51 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("surpriseBtn").addEventListener("click", () => {
+    const surpriseElement = document.getElementById("surprise");
+    surpriseElement.classList.toggle("hidden");
+  });
+  
