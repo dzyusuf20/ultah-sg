5b460b20ca5f42e259dd3697c3a0c900207379c3
dzyusuf20 <Yusufdeardo20@gmail.com>
2024-11-26 04:12:37 +0700

    Fix CSS and JS issues

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const hero = document.querySelector('.hero');
 document.addEventListener('mousemove', (e) => {
+    const hero = document.querySelector('.hero');
     let x = (e.clientX / window.innerWidth) * 100;
     let y = (e.clientY / window.innerHeight) * 100;
     hero.style.backgroundPosition = `${x}% ${y}%`;
