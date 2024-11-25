e2d2d35cf77acde258e50f60e2fc8191d907d38e
dzyusuf20 <Yusufdeardo20@gmail.com>
2024-11-26 02:00:23 +0700

    Update HTML dan CSS dengan tampilan baru

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-document.getElementById("surpriseBtn").addEventListener("click", () => {
-    const surpriseElement = document.getElementById("surprise");
-    surpriseElement.classList.toggle("hidden");
-  });
-  
+// Play music
+const music = document.getElementById('birthdayMusic');
+music.volume = 0.5;
+
+// Surprise Button
+const surpriseButton = document.getElementById('surpriseButton');
+const surpriseSection = document.querySelector('.surprise-section');
+const fireworks = document.querySelector('.fireworks');
+
+surpriseButton.addEventListener('click', () => {
+    // Show Surprise
+    surpriseSection.style.display = 'block';
+    fireworks.style.display = 'block';
+    
+    // Stop music if needed
+    if (music.paused) {
+        music.play();
+    }
+});
